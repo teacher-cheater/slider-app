5a2898554fcda77440b77f84a5559b78a986ff1a
teacher_cheater <johnnykayfun@gmail.com>
2022-09-26 21:05:15 +0300

    add styles css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import axios from "axios";
+import "./App.css";
+
+//axios.get('https://slider.ymatuhin.workers.dev/', {
+//  params: {
+//    ID: 12345
+//  }
+//})
+//.then(function (response) {
+//  console.log(response);
+//})
+//.catch(function (error) {
+//  console.log(error);
+//});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" className="active" data-bs-target />
+          <button type="button" data-bs-target />
+          <button type="button" data-bs-target />
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://picsum.photos/200/300"
+              className="d-block w-100"
+              height="400px"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://picsum.photos/200/300"
+              className="d-block w-100"
+              height="400px"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://picsum.photos/200/300"
+              className="d-block w-100"
+              height="400px"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button">
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Предыдущий</span>
+        </button>
+        <button className="carousel-control-next" type="button">
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Следующий</span>
+        </button>
+      </div>
     </div>
   );
 }
